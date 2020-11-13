fd6e7ccfe15fff793511cb699c1e061844f389a3
Rutaceae <jamiemoeller@yahoo.dk>
2020-11-13 23:49:30 +0100

    Added events, preliminary enemy/stepunit work.

---
+++ Assets/Scripts/MeleeEnemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeEnemy : StepUnit
+{
+    
+    private void Update()
+    {
+
+    }
+
+
+    public override void OnStep()
+    {
+        base.OnStep();
+
+        Debug.Log("step");
+        
+        //Do step-stuff.
+        //if(grid.isPlayerOnPos) 
+            //Needs grid-reference to kill player.
+
+
+    }
+
+}
---
+++ Assets/Scripts/StepUnit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StepUnit : MonoBehaviour
+{
+    public bool shouldStep;
+
+    public virtual void OnStep()
+    {
+        if (!shouldStep)
+        {
+            return;
+        }
+
+        //Do other things in classes.
+    }
+
+
+}
