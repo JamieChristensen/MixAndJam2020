015bcd34057fc7cff9ed7d633ffc93b0de188330
Oliver Jørgensen <OliverJ190196@gmail.com>
2020-11-14 20:55:25 +0100

    Looping ranged enemy

---
+++ Assets/Scripts/LoopingRangedEnemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopingRangedEnemy : RangedEnemy
+{
+    public override bool IsPrepareStep()
+    {
+        int PrepareStep = ShootStep - IndicatorSteps;
+        return GameManager.instance.stepCount >= PrepareStep && GameManager.instance.stepCount % ShootStep == PrepareStep;
+    }
+
+    public override bool IsShootStep() => GameManager.instance.stepCount >= ShootStep && GameManager.instance.stepCount % ShootStep == 0;
+}
