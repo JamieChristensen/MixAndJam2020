afae5a70101cd2fac5096cef7196cd5f36fd8845
darkpanix <38384941+darkpanix@users.noreply.github.com>
2020-11-14 20:18:23 +0100

    added marker to prefabs

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FeedbackTest : MonoBehaviour
+public class AudioManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,7 +14,5 @@ public class FeedbackTest : MonoBehaviour
     void Update()
     {
         
-        
-
     }
 }
