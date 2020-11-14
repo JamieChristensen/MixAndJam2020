098f9c38638543d4d28c796f261ed308883fc69d
Rutaceae <jamiemoeller@yahoo.dk>
2020-11-14 13:44:33 +0100

    Basic stepfunctionality

--- Assets/Scripts/Small event-reaction scripts/ChangeMaterialColor.cs
+++ Assets/Scripts/Small event-reaction scripts/ChangeMaterialColor.cs
@@ -8,11 +8,39 @@ public class ChangeMaterialColor : MonoBehaviour
     private MeshRenderer meshRenderer;
 
     [SerializeField]
+    private string propertyName = "_BaseColor";
+
+    [SerializeField]
     [ColorUsage(true, true)]
     private Color colorToChangeTo;
 
+
+    [Tooltip("Only used if changeBetweenTwoColors is true")]
+    [SerializeField]
+    [ColorUsage(true, true)]
+    private Color secondColorToChangeTo;
+
+
+    public bool changeBetweenTwoColors = false;
+    private bool isColorTwoChosen = false;
+
     public void ChangeColor()
     {
-        meshRenderer.material.color = colorToChangeTo;
+        if (!changeBetweenTwoColors)
+        {
+            meshRenderer.material.SetColor("_BaseColor", colorToChangeTo);
+            return;
+        }
+
+        if (isColorTwoChosen)
+        {
+            meshRenderer.material.SetColor("_BaseColor", colorToChangeTo);
+            isColorTwoChosen = false;
+        }
+        else
+        {
+            meshRenderer.material.SetColor("_BaseColor", secondColorToChangeTo);
+            isColorTwoChosen = true;
+        }
     }
 }
