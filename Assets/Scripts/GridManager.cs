d2f82c8b48556580de4634ec16a853167779ddb4
Oliver Jørgensen <OliverJ190196@gmail.com>
2020-11-14 13:00:15 +0100

    Grid- and LevelManager to keep track of levels and spawn the grid

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,18 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Mathematics;
+using Sirenix.OdinInspector;
 
-public class GridManager : MonoBehaviour
+public class GridManager : SerializedMonoBehaviour
 {
+    [SerializeField]
+    private LevelManager LevelManager;
+
+    private float2 CellSize;
+    private Cell[,] Cells;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Level CurrentLevel = LevelManager.GetCurrentLevel();
+        Cell Cell = CurrentLevel.Grid[0, 0];
+
+        var Renderer = Cell.GetComponent<Renderer>();
+        var Bounds = Renderer.bounds; 
+        var BoundsDistance = Bounds.max - Bounds.min;
+        CellSize.x = BoundsDistance.x;
+        CellSize.y = BoundsDistance.z;
+
+
+        GenerateGrid(CurrentLevel);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void GenerateGrid(Level Level) {
+        int LevelSizeX = Level.Grid.GetLength(0);
+        int LevelSizeY = Level.Grid.GetLength(1);
+
+        Cells = new Cell[LevelSizeX, LevelSizeY];
+
+        for (int i = 0; i < LevelSizeX; i++)
+        {
+            for (int j = 0; j < LevelSizeY; j++)
+            {
+                Cells[i, j] = Instantiate(Level.Grid[i, j], new Vector3(CellSize.x * i, 0, CellSize.y * j), Quaternion.identity, this.transform);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+[CreateAssetMenu()]
+public class LevelManager : SerializedScriptableObject
+{
+    [SerializeField]
+    private Level[] AllLevels;
+
+    public int CurrentLevel;
+
+    public Level GetCurrentLevel() => AllLevels[CurrentLevel];
+
+
+}
