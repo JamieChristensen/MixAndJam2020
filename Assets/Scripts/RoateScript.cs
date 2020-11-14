eedbad21565e3a16624109c8370beb339304ed5e
darkpanix <38384941+darkpanix@users.noreply.github.com>
2020-11-14 22:19:11 +0100

    Ranged projectile - aka the real OG bullet - has landed

---
+++ Assets/Scripts/RoateScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoateScript : MonoBehaviour
+{
+    public Vector3 rotation = new Vector3(1,0,1);
+
+
+    void Update()
+    {
+        transform.Rotate(rotation, Space.Self);
+    }
+}
