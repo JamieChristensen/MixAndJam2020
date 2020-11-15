d9b39e741ff0fc1a76adc02c02f9d8d79e56a2d2
Oliver Jørgensen <OliverJ190196@gmail.com>
2020-11-15 14:21:22 +0100

    Feedback canvasu

---
+++ Assets/Scripts/FeedbackCanvas.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using TMPro;
+using Sirenix.OdinInspector;
+
+public class FeedbackCanvas : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI TextObject;
+
+    public void SetText(string Text)
+    {
+        TextObject.text = Text;
+    }
+
+    [Button(ButtonSizes.Large)]
+    [ContextMenu("BOOM")]
+    public void ActivateFeedback()
+    {
+        // Default 
+        var DefaultScale = TextObject.rectTransform.localScale;
+        DefaultScale.y = 0;
+        TextObject.rectTransform.localScale = DefaultScale;
+        TextObject.alpha = 1;
+
+        // BAM! In!
+        TextObject.rectTransform.DOScaleY(1, 0.2f);
+        TextObject.DOFade(1.0f, 0.2f);
+        TextObject.rectTransform.DOShakeRotation(0.4f, 40, 5);
+
+        // Slow fade out
+        DeactivateFeedback();
+    }
+
+    [Button(ButtonSizes.Large)]
+    [ContextMenu("MOOB")]
+    public void DeactivateFeedback()
+    {
+        TextObject.DOFade(0.0f, 2.0f);
+    }
+}
