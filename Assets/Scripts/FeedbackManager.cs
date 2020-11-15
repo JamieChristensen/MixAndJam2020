994e10cbc125ebe0d4c09e446a7f168c14163c28
darkpanix <38384941+darkpanix@users.noreply.github.com>
2020-11-15 13:06:24 +0100

    Hurt warning for arrow

--- Assets/Scripts/FeedbackManager.cs
+++ Assets/Scripts/FeedbackManager.cs
@@ -3,42 +3,88 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 using DG.Tweening;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(AudioSource))]
 public class FeedbackManager : MonoBehaviour
 {
     [SerializeField]
-    AudioClip clip = default;
-
+    AudioClip oneBeatAudio = default;
     [SerializeField]
     CinemachineVirtualCamera followCamera = default;
-
     [SerializeField]
-    float cameraShakeAmount = 1f, shakeDuration = 1f;
-
+    float beastShake = 1f, deathShake = 2f, shakeDuration = .2f;
+    [SerializeField]
+    Image panelWarning;
+    [SerializeField]
+    Color PanelStartColor, warningstartColor;
+    [SerializeField]
+    float panelWarningSpeed = 1.2f;
     AudioSource audSource;
     CinemachineBasicMultiChannelPerlin cameraNoiseChannel;
 
+     bool lerpPanel;
+    float startLerp = 0;
+
     void Start()
     {
-        cameraNoiseChannel = followCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        if (followCamera != null)
+        {
+            cameraNoiseChannel = followCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        }
+        
         audSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        
+        if (lerpPanel)
+        {
+            startLerp += Time.deltaTime * panelWarningSpeed;
+            panelWarning.color = Color.Lerp(PanelStartColor, warningstartColor, startLerp);
+        }
     }
 
     public void playClipOnBeat()
     {
-        audSource.PlayOneShot(clip);
+        audSource.PlayOneShot(oneBeatAudio);
     }
 
     [ContextMenu("shake that camera booty")]
-    public void CameraShake()
+    public void BeatShake()
     {
-        StartCoroutine(TimeDelay());
+        StartCoroutine(BeatCameraShake());
+    }
+    [ContextMenu("shake that camera booty")]
+    public void DeathShake()
+    {
+        StartCoroutine(DeathCameraShake());
     }
 
-    IEnumerator TimeDelay()
+    [ContextMenu("testWarningPanel")]
+    public void WarningArrowIncoming()
     {
-        cameraNoiseChannel.m_AmplitudeGain = cameraShakeAmount;
+        lerpPanel = true;
+    }
+    [ContextMenu("testDeflectedArrow")]
+    public void DeflectedArrow()
+    {
+        lerpPanel = false;
+        panelWarning.color = PanelStartColor;
+    }
+
+
+    IEnumerator DeathCameraShake()
+    {
+        cameraNoiseChannel.m_AmplitudeGain = deathShake;
+        yield return new WaitForSeconds(shakeDuration+ .5f);
+        cameraNoiseChannel.m_AmplitudeGain = 0;
+    }
+
+    IEnumerator BeatCameraShake()
+    {
+        cameraNoiseChannel.m_AmplitudeGain = beastShake;
         yield return new WaitForSeconds(shakeDuration);
         cameraNoiseChannel.m_AmplitudeGain = 0;
     }
